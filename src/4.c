d51d0cec421bc944af0e9f1e03750b5bc2e74435
croslincregan688 <croslincregan688@gmail.com>
2025-03-09 07:42:45 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    char name[] = "John Doe";
+    float height = 1.80f;
+
+    printf("Hello, my name is %s and I am %d years old.\n", name, age);
+    printf("I am %f meters tall.", height);
+
+    return 0;
+}
