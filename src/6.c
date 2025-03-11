b97fff4ff038a2f76050cf5d0ab07226f4ff8af0
croslincregan688 <croslincregan688@gmail.com>
2025-03-11 21:21:52 +0000

    Add 6.c

---
+++ src/6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int x = 5;
+  int y = 7;
+  int z = 3;
+  if (x > y) {
+    printf("x is greater than y\n");
+  } else {
+    printf("x is less than or equal to y\n");
+  }
+  return 0;
+}
