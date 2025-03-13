7647fa9f3ba722998c91829a564f8f855bc21fd8
croslincregan688 <croslincregan688@gmail.com>
2025-03-13 20:19:17 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("Sorry, you are not eligible to vote yet.\n");
+    }
+    return 0;
+}
