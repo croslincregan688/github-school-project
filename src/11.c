d2076b98c37d67a86444d577695592327d0a7854
croslincregan688 <croslincregan688@gmail.com>
2025-03-16 03:50:03 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,12 @@
+
+int main() {
+    // Declare variables and initialize them to some values
+    int x = 5;
+    float y = 3.14;
+    char z = 'a';
+
+    // Print the values of the variables
+    printf("x: %d\ny: %f\nz: %c\n", x, y, z);
+
+    return 0;
+}
